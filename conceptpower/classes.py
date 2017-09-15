d88de4c1a51094a685b7c97e4db69d799c9dfc26
Pranav Sharan <psharan@digitals-mbp.mobile.asu.edu>
2017-09-15 10:35:22 -0700

    [VGNWB-216] Fixed VIAF id not added to equal to field when approving concepts

--- conceptpower/classes.py
+++ conceptpower/classes.py
@@ -236,8 +236,8 @@ class Conceptpower:
             "description": description,
             "type": concepttype,
             "synonymids": synonym_ids,
-            "equal_to": ",".join(str(equal_uris_element) for equal_uris_element in equal_uris),
-            "similar": ",".join(str(similar_uris_element) for similar_uris_element in similar_uris)
+            "equal_to": ",".join(equal_uris_element for equal_uris_element in equal_uris),
+            "similar": ",".join(similar_uris_element for similar_uris_element in similar_uris)
         }
 
         r = requests.post(url=rest_url, data=json.dumps(concept_data), auth=auth)
