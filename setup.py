267c3c704eaea2d25c625acd45cfbde41c7136ac
Taylor Quinn <tjquinn1@asu.edu>
2019-08-05 11:46:23 -0700

    fixed email

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from distutils.core import setup
 
 DISTNAME = 'conceptpower-python3-api'
 AUTHOR = 'E. Peirson, Digital Innovation Group @ ASU'
+AUTHOR_EMAIL = '"E. Peirson" <erick.peirson@asu.edu>'
 MAINTAINER = 'T. Quinn Digital Innovation Group @ ASU'
-MAINTAINER_EMAIL = "T. Quinn" <tjquinn1@asu.edu>
+MAINTAINER_EMAIL = '"T. Quinn" <tjquinn1@asu.edu>'
 DESCRIPTION = ('A very simple library for querying a Conceptpower REST API.')
 LICENSE = 'GNU GPL 3'
 URL = 'https://github.com/erickpeirson/Conceptpower-API'
@@ -14,7 +15,7 @@ PACKAGES = [ 'conceptpower',]
 setup(
     name=DISTNAME,
     author=AUTHOR,
-    author_email=MAINTAINER_EMAIL,
+    author_email=AUTHOR_EMAIL,
     maintainer=MAINTAINER,
     maintainer_email=MAINTAINER_EMAIL,
     description=DESCRIPTION,
